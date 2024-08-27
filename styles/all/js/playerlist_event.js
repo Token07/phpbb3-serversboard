ef83cf614641729bb9708ece6715427f28a9bb19
headshoter <chdk2011@gmail.com>
2024-08-27 08:31:59 -0300

    Fix error 500 on phpbb 3.3.9 and forth
    Directory no longer neccesary

--- styles/all/js/playerlist_event.js
+++
@@ -1,9 +0,0 @@
-phpbb.addAjaxCallback('token07.serversboard.players', function(response) {
-    var player_list;
-	
-	player_list = $('<ul style="list-style: none" /></ul>')
-	$.each(response.PLAYER_LIST, function(i, v) {
-		$('<li/>').text(v.Name).appendTo(player_list)
-	})
-	phpbb.alert(response.TITLE, player_list)
-});
